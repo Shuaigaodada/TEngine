f815e1dac84072abe5f76d77b1b982deda43b4de
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-02-07 14:38:04 +0000

    导入所有组件，并封装到Engine

--- TEngine/TEngine.py
+++ TEngine/TEngine.py
@@ -3,16 +3,38 @@ __all__ = ["TEngine"]
 
 import sys
 import atexit
+import typing as T
 import unicurses as curses
-from .fileLogger import DebugLogger
+from Input import Input
+from Screen import Screen
+from Renderer import Renderer
+from fileLogger import DebugLogger
+
+"""
+TEngine应当有几个类实例
+    screen对象: 用于绘制字符
+    renderer对象: 用于绘制颜色色块
+    input对象: 用于获取输入
+    mouse对象: 用于获取鼠标输入
+    logger对象: 用于记录日志
+"""
 
 class TEngine:
     def __init__(self) -> None:
-        self.mainWin = curses.stdscr
-        self.logger: DebugLogger = None
+        self.stdscr = curses.stdscr
+        self.logger: DebugLogger = None if not DebugLogger.instance else DebugLogger.instance[0]
+        self.renderer = Renderer()
+        self.screen = Screen(self.stdscr)
+        self.input = Input(self.stdscr)
         return
     
-    def init(self, registerExit: bool = True) -> None:
+    def SetLogger(self, logger: DebugLogger) -> None:
+        """设置日志记录器"""
+        self.logger = logger
+        self.renderer.SetLogger(logger)
+        return
+    
+    def Init(self, registerExit: bool = True) -> None:
         """
         初始化引擎, 进入cbreak和noecho模式, 设置光标不可见, 初始化颜色。
         
@@ -24,16 +46,14 @@ class TEngine:
         curses.noecho()
         curses.curs_set(0)
         curses.start_color()
+        curses.keypad(self.stdscr, True)
         
         if registerExit:
             # 注册退出事件
-            atexit.register(self.exit)
-
-        self.mainWin = curses.stdscr
-        
+            atexit.register(self.Exit)
         return
         
-    def exit(self) -> None:
+    def Exit(self) -> None:
         """
         卸载引擎, 退出cbreak和noecho模式, 显示光标。
         """
@@ -42,25 +62,29 @@ class TEngine:
         curses.echo()
         curses.endwin()
         
+        # 获取异常信息
         excType, excValue, excTraceback = sys.exc_info()
+        
+        # 关闭日志并且判断是否有异常并输出
         if self.logger is not None:
             if excType is not None:
-                self.logger.error("An error occurred:", excType, excValue, excTraceback)
+                self.logger.Error("An error occurred:", excType, excValue, excTraceback)
             else:
-                self.logger.info("Exited successfully.")
-            self.logger.close()
-        
+                self.logger.Info("Exited successfully.")
+            self.logger.Close()
         return
     
-    def nodelayMode(self, mode: bool) -> None:
+    def NoDelayMode(self, mode: bool) -> None:
         """
         设置是否阻塞输入。
         
         参数:
             mode: bool, 是否阻塞输入
         """
-        curses.nodelay(self.mainWin, mode)
+        curses.nodelay(self.stdscr, mode)
         return
-        
+    
+
+
     
 
