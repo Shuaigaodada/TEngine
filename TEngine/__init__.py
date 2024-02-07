aedb6b2bf353f73de7ccf089883182f39e604b47
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-02-07 14:35:43 +0000

    添加更多模块

--- TEngine/__init__.py
+++ TEngine/__init__.py
@@ -1,9 +1,11 @@
 from .fileLogger import DebugLogger
 from .TEngine import TEngine
+from .resource import Resource, FileLoader
 
 version = "0.0.0beta"
 __all__ = [
     "version",
     "DebugLogger",
-    "TEngine"
+    "TEngine",
+    "Resource", "FileLoader"
 ]
