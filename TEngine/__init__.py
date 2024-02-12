fded1e1b9c1cfe1dcde83563721cacd501f89851
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-02-12 21:40:01 +0000

    一些结构目录修改

--- TEngine/__init__.py
+++ TEngine/__init__.py
@@ -1,6 +1,6 @@
-from .fileLogger import DebugLogger
-from .TEngine import TEngine
-from .resource import Resource, FileLoader
+from .Engine.fileLogger import DebugLogger
+from .Engine.TEngine import TEngine
+from .Engine.resource import Resource, FileLoader
 
 version = "0.0.0beta"
 __all__ = [
