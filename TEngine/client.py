808a82b9a6ccd39278e831c740ecb494f413f3d0
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-03-05 15:02:56 +0000

    client组件

--- TEngine/client.py
+++ TEngine/client.py
@@ -1,13 +1,48 @@
 import os
+import time
 import json
 import pickle
 import socket
 import struct
 from typing import *
 
-__all__ = ['Client']
+__all__ = ['SocketClient']
 
-class Client:
+class ReturnedBytes(NamedTuple):
+    data: bytes
+    
+    @property
+    def size( self ) -> int:
+        return len( self.data )
+    def __len__( self ) -> int:
+        return self.size
+    
+    def as_json( self, encoding: str = "utf-8", *args: Tuple[Any], **kwargs: Dict[str, Any] ) -> Dict[str, Any]:
+        return json.loads( self.data.decode( encoding ), *args, **kwargs )
+    def decode( self, encoding: str = "utf-8", __error: str = "strict" ) -> str:
+        return self.data.decode( encoding, __error )
+    
+    # same for as_json (as_list, as_dict)
+    def as_list( self, encoding: str = "utf-8", *args: Tuple[Any], **kwargs: Dict[str, Any] ) -> List[Any]:
+        return self.as_json( encoding, *args, **kwargs )
+    def as_dict( self, encoding: str = "utf-8", *args: Tuple[Any], **kwargs: Dict[str, Any] ) -> Dict[str, Any]:
+        return self.as_json( encoding, *args, **kwargs )
+    # end
+    
+    def as_object( self, 
+                    fix_imports : bool  = True, 
+                    encoding    : str   = "utf-8", 
+                    errors      : str   = "strict",
+                    buffers     : bool  = None 
+                    ) -> Any:
+        
+        return pickle.loads( self.data, fix_imports, encoding, errors, buffers )
+    
+    @property
+    def bytes( self ) -> bytes:
+        return self.data
+
+class SocketClient:
     def __init__( self, 
                  host_address   : str       = "localhost", 
                  port           : int       = 8000,
@@ -27,7 +62,7 @@ class Client:
         socket_protocol = proto_info[1]
         
         self.socket = socket.socket( socket_family, socket_protocol )
-        self.__buffers: Dict[ int, bytes ] = { }
+        self.__size_buffer: int = -1
 
     def connect( self, __timeout: Optional[int] = None, __retry: int = 3 ) -> None:
         """连接服务器"""
@@ -42,6 +77,7 @@ class Client:
                 if i == __retry - 1:
                     raise e
                 else:
+                    time.sleep( 1 )
                     continue
         self.socket.settimeout( None )
     
@@ -68,16 +104,47 @@ class Client:
              __size: int = -1, 
              __flags: int = 0, 
              __timeout: Optional[int] = None 
-             ) -> Any:
+             ) -> ReturnedBytes:
         
         recv: Callable = self.socket.recv if self.protocol == "TCP" else self.socket.recvfrom
         
         self.socket.settimeout( __timeout )
-        if not __size:
-            bsize: float = struct.unpack( ">L", recv( struct.calcsize( ">L" ), __flags ) )[ 0 ]
-            return recv( bsize, __flags )
+        if __size == -1:
+            if self.__size_buffer == -1:
+                try:
+                    bsize: int = struct.unpack( ">L", recv( struct.calcsize( ">L" ), __flags ) )[ 0 ]
+                    self.__size_buffer = bsize
+                except struct.error:
+                    return None # TODO: 处理struct.unpack异常
+                except socket.timeout:
+                    return None # TODO: 处理超时
+                except BlockingIOError:
+                    return None # TODO: 处理阻塞
+            
+            try:
+                data: bytes = recv( self.__size_buffer, __flags )
+                self.__size_buffer = -1
+                return ReturnedBytes( data )
+            except socket.timeout:
+                return None # TODO: 处理超时
+            except BlockingIOError:
+                return None # TODO: 处理阻塞
+            
         else:
             return recv( __size, __flags )
+        
+    def __enter__( self ) -> "SocketClient":
+        return self
+    def __exit__( self, exc_type, exc_value, traceback ) -> None:
+        self.close()
+        
+    def close( self ) -> None:
+        """关闭连接"""
+        try:
+            self.socket.shutdown( socket.SHUT_RDWR )
+            self.socket.close()
+        except OSError:
+            return
     
     def encode( self, __data: Any, encoding: str = "utf-8" ) -> bytes:
         """编码数据"""
@@ -88,7 +155,7 @@ class Client:
         elif isinstance( __data, (list, dict) ):
             return json.dumps( __data ).encode( encoding )
         elif isinstance( __data, (int, float) ):
-            return struct.pack( ">L", __data )[ 0 ]
+            return struct.pack( ">L", __data )
         else:
             return pickle.dumps( __data )
           
@@ -144,4 +211,32 @@ class Client:
             raise ValueError( f"Invalid protocol: {__proto}" )
         
         return proto_str, __proto
-   
+
+
+if __name__ == "__main__":
+    from threading import Thread
+    
+    def server( ) -> None:
+        from server import SocketServer
+        with SocketServer( ) as server:
+            server.listen( )
+            
+            server.accept_for( 1 )
+            
+            server.send( b"Hello Client" )
+            
+            data = server.recv( )[ 0 ]
+            print( data.as_json( ) )
+    
+    Thread( target = server ).start( )
+    
+    with SocketClient( ) as client:
+        time.sleep( 1 )
+        client.connect( )
+        
+        client.send( [1, 2, 3, 4] )
+        
+        data = client.recv( )
+        print( data.decode( ) )
+        
+    
