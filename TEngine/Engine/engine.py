a2cf6f2ae9d8b128867d93bd09098bff0dc0f653
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-04-25 14:06:08 +0000

    修复日志输出错误，格式化

--- TEngine/Engine/engine.py
+++ TEngine/Engine/engine.py
@@ -10,7 +10,7 @@ from ..interfaces import Engine as IEngine
 
 class Engine(IEngine):
     def __new__(cls, *args, **kwargs):
-        instance = super().__new__(cls)
+        instance = super().__new__( cls )
         instance.__init()
         return instance
     
@@ -18,23 +18,23 @@ class Engine(IEngine):
         self.stdscr: Optional[curses.window] = None
         self.logger: Optional[IFileLogger] = None
         
-        self.renderer: IRenderer = IRenderer()
-        self.screen: IScreen = IScreen()
-        self.input: IInput = IInput()
+        self.renderer: IRenderer =      IRenderer   ( )
+        self.screen: IScreen =          IScreen     ( )
+        self.input: IInput =            IInput      ( )
     
-    def set_logger(self) -> None:
-        self.logger = IFileLogger( )
-        self.renderer.set_logger(self.logger)
-        self.screen.set_logger(self.logger)
-        self.input.set_logger(self.logger)
-        self.input.mouse.set_logger(self.logger)
+    def set_logger(self, path: Optional[str] = None) -> None:
+        self.logger = IFileLogger       ( path )
+        self.renderer.set_logger        ( self.logger )
+        self.screen.set_logger          ( self.logger )
+        self.input.set_logger           ( self.logger )
+        self.input.mouse.set_logger     ( self.logger )
         
     def set_screen( self, stdscr: curses.window ) -> None:
         self.stdscr = stdscr
-        self.renderer.set_screen(stdscr)
-        self.screen.set_screen(stdscr)
-        self.input.set_screen(stdscr)
-        self.input.mouse.set_screen(stdscr)
+        self.renderer.set_screen        ( stdscr )
+        self.screen.set_screen          ( stdscr )
+        self.input.set_screen           ( stdscr )
+        self.input.mouse.set_screen     ( stdscr )
         
     def init_engine(self, __reg: bool = True) -> None:
         self.set_screen( curses.initscr() )
@@ -52,10 +52,11 @@ class Engine(IEngine):
         curses.echo             ( )
         curses.nocbreak         ( )
         curses.endwin           ( )
-        print("\033[?1003l", end="", flush=True)
+        print("\033[?1003l", end="", flush=True) # close mouse tracking
         
         if self.logger is not None:
             try: self.logger.info("Engine quit")
+            except ValueError: pass # ValueError: I/O operation on closed file
             finally:
                 try:                self.logger.close()
                 except Exception:   pass
