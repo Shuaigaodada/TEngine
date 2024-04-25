31df77410e1db32e6a26f7e63c1e81ff9c5f71ee
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2024-04-25 14:02:29 +0000

    导入CursesWindow类型

--- TEngine/__init__.py
+++ TEngine/__init__.py
@@ -1,8 +1,10 @@
 from . import interfaces
 from .Engine import engine_component
 from .Engine.engine import Engine
+from curses import window as CursesWindow
 __all__ = [
     "Engine",
     "interfaces",
-    "engine_component"
+    "engine_component",
+    "CursesWindow"
 ]
